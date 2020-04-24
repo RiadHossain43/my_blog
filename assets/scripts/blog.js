cef85df17ca264e0e81888e655ac37c7cc7c87e0
RiadHossain <riadhossain7464@gmail.com>
2020-04-24 16:39:01 +0600

    Animation update

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -1,8 +1,11 @@
 let eleID = (id) => {
     return document.getElementById(id);
 }
+let eleCls = (cls) => {
+    return document.getElementsByClassName(cls);
+}
 let set_style = (elem, styles) => {
-    Object.assign(elem.style, styles)
+    Object.assign(elem.style, styles);
 }
 
 window.addEventListener('load', () => {
@@ -19,8 +22,8 @@ window.addEventListener('load', () => {
                 let logo = eleID('logo');
                 let hamberger = eleID('hamberger');
                 set_style(greet, { animation: 'greetInUp .75s', opacity: .8 });
-                set_style(logo,{animation:"tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both"});
-                set_style(hamberger,{animation:"tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both"})
+                set_style(logo, { animation: "tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both" });
+                set_style(hamberger, { animation: "tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both" })
             }
             greetAnim();
         }, 800);
@@ -41,12 +44,18 @@ window.addEventListener('scroll', () => {
             set_style(nav, { backgroundColor: 'transparent', boxShadow: '0 6px 20px black' });
     }
     let blogAnim = () => {
-        let blogs = document.getElementsByClassName('blog');
-        for (var i = 0; i < blogs.length; i++) {
+        let blogs = eleCls('blog');
+        let read = eleCls('read');
+        for (let i = 0; i < blogs.length; i++) {
             let blog = blogs[i].getBoundingClientRect();
-
             if (blog.top <= window.innerHeight * .75) {
                 set_style(blogs[i], { opacity: 1, animation: 'fadeInUp .75s' })
+            }
+            if (window.innerWidth <= 768) {
+                if (blog.top >= 0 && blog.bottom <= (window.innerHeight *1.15 || document.documentElement.clientHeight *1.15)) 
+                    set_style(read[i], { width: '75%', height: '75%', opacity: '.6' });
+                else 
+                    set_style(read[i], { width: '95%', height: '95%', opacity: '0' });
             }
         }
     }
@@ -73,22 +82,24 @@ let menuHandler = () => {
     let hamberger = eleID('hamberger');
     let menu = eleID('menu');
     let back = eleID('backlink');
-    let link = document.getElementsByClassName('link');
+    let link = eleCls('link');
     hamberger.addEventListener('click', () => {
-        set_style(menu, { clipPath: "circle(2200px at 100% 0%)" });
+        set_style(menu, { clipPath: "circle(1200px at 100% 0%)" });
         let animDelay = .3;
-        for(var i=0;i<link.length;i++){
-            set_style(link[i],{opacity:1,animation:`bounce-top 0.9s both ${animDelay}s`});
-            animDelay+=.3;
+        for (var i = 0; i < link.length; i++) {
+            set_style(link[i], { opacity: 1, animation: `bounce-top 0.9s both ${animDelay}s` });
+            animDelay += .3;
         }
     });
     back.addEventListener('click', () => {
         set_style(menu, { clipPath: "circle(0px at 100% 0%)" });
-        for(var i=0;i<link.length;i++){
-            set_style(link[i],{opacity:0,animation:""});
+        for (var i = 0; i < link.length; i++) {
+            set_style(link[i], { opacity: 0, animation: "" });
         }
     });
 }
+
 // function calls...
 
 menuHandler();
+
